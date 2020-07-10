383ae5f4d58e1810b329e01961d1352c7c430b53
b3nj4m1n <b3nj4m1n@gmx.net>
2020-07-10 16:32:57 +0200

    Added example color schemes

---
+++ res/examples.js
@@ -0,0 +1,14 @@
+exampleColors = [
+    {
+        "name": "dracula",
+        "value": "! Dracula Xresources palette\n*.foreground: #F8F8F2\n*.background: #282A36\n*.color0:     #000000\n*.color8:     #4D4D4D\n*.color1:     #FF5555\n*.color9:     #FF6E67\n*.color2:     #50FA7B\n*.color10:    #5AF78E\n*.color3:     #F1FA8C\n*.color11:    #F4F99D\n*.color4:     #BD93F9\n*.color12:    #CAA9FA\n*.color5:     #FF79C6\n*.color13:    #FF92D0\n*.color6:     #8BE9FD\n*.color14:    #9AEDFE\n*.color7:     #BFBFBF\n*.color15:    #E6E6E6\n"
+    },
+    {
+        "name": "nord",
+        "value": 'scheme: "Nord"\nauthor: "arcticicestudio"\nbase00: "2E3440"\nbase01: "3B4252"\nbase02: "434C5E"\nbase03: "4C566A"\nbase04: "D8DEE9"\nbase05: "E5E9F0"\nbase06: "ECEFF4"\nbase07: "8FBCBB"\nbase08: "88C0D0"\nbase09: "81A1C1"\nbase0A: "5E81AC"\nbase0B: "BF616A"\nbase0C: "D08770"\nbase0D: "EBCB8B"\nbase0E: "A3BE8C"\nbase0F: "B48EAD"\n'
+    },
+    {
+        "name": "gruvbox",
+        "value": "hard contrast: *background: #1d2021\n*background: #282828\n! soft contrast: *background: #32302f\n*foreground: #ebdbb2\n! Black + DarkGrey\n*color0:  #282828\n*color8:  #928374\n! DarkRed + Red\n*color1:  #cc241d\n*color9:  #fb4934\n! DarkGreen + Green\n*color2:  #98971a\n*color10: #b8bb26\n! DarkYellow + Yellow\n*color3:  #d79921\n*color11: #fabd2f\n! DarkBlue + Blue\n*color4:  #458588\n*color12: #83a598\n! DarkMagenta + Magenta\n*color5:  #b16286\n*color13: #d3869b\n! DarkCyan + Cyan\n*color6:  #689d6a\n*color14: #8ec07c\n! LightGrey + White\n*color7:  #a89984\n*color15: #ebdbb2\n"
+    },
+]
